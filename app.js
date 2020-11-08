2a9abd42cfe19acf593bddc1d822309b484deaa1
sergio castillo <scastillonu@uniminuto.edu.co>
2020-11-08 18:08:25 -0500

    ultimos arreglos

--- app.js
+++ app.js
@@ -1,33 +1,54 @@
-const express = require('express');
-const app = express();
-const morgan = require("morgan");
-const errorHandler=require("./middleware/error");
-const categoryRoutes = require('./routes/categories');
-mongoose = require('mongoose');
+const express = require('express')
+const path = require('path')
+const dotenv = require('dotenv')
+const colors = require('colors')
+const morgan = require('morgan')
 
-database = require('./config/db');
 
-//Connect mongoDB
-mongoose.Promise = global.Promise;
-mongoose.connect(database.db, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log("Database connected")
-},
-    error => {
-        console.log("Database could't be connected to: " + error)
-    })
+const errorHandler = require('./middleware/error')
 
-app.use(express.json());
-if (process.env.NODE_ENV === "development") {
-    app.use(morgan('dev'));
+const DBConnection = require('./config/db')
+
+dotenv.config({ path: './config/.env' })
+
+DBConnection()
+
+
+const categoryRoutes = require('./routes/categories')
+
+const app = express()
+
+app.use(express.json())
+
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
 }
-const dotenv = require("dotenv");
 
-app.use('/api/v1/categories', categoryRoutes);
+
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+
+const versionOne = (routeName) => `/api/v1/${routeName}`
+
+
+app.use(versionOne('categories'), categoryRoutes)
+
 app.use(errorHandler)
-const PORT = process.env.PORT || 4000
-app.listen(PORT, () => {
-    console.log("Sistema corriendo en puerto " + PORT)
+
+const PORT = process.env.PORT
+
+const server = app.listen(PORT, () => {
+    console.log(
+        `We are live on ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+    )
+})
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red)
+    // Close server & exit process
+    server.close(() => process.exit(1))
 })
